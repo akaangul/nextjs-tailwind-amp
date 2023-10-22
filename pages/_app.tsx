3eadfc9ecb06f13ade69010bc857d387a6eeb3c0
Fei <iamkaangul@gmail.com>
2023-10-23 00:12:30 +0300

    qwewq
    qwewq

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    // Tarayıcı URL'sini doğrulayarak sadece Google'dan gelen ziyaretçilere izin verin
+    if (document.referrer !== 'https://www.google.com/') {
+      // Google'dan gelmeyen ziyaretçilere hata sayfasına yönlendirin
+      router.push('/error');
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/error.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Üzgünüz, bu sayfaya erişim izniniz yok.</h1>
+      <p>Sadece Google'dan gelen ziyaretçilere izin verilmektedir.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
