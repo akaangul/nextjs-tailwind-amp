e1f123cf6480b89fc0066fce7a6616c05877fdd2
Fei <iamkaangul@gmail.com>
2023-10-23 00:17:40 +0300

    qweqw

--- next.config.js
+++ next.config.js
@@ -9,18 +9,4 @@ const config = {
   },
 }
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://www.google.com', // Sadece Google'dan gelen ziyaretçilere izin vermek için Google'ın ana sayfasını ekleyin
-          },
-        ],
-      },
-    ];
-  },
-};
+module.exports = config
